e6e3311a93269bea59947e62ab748b77a9dcad81
Justin <merrelljustin@gmail.com>
2022-02-09 21:03:08 +0000

    Allowed Host Override

--- VCHIP/VCHIP/settings.py
+++ VCHIP/VCHIP/settings.py
@@ -36,8 +36,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = os.getenv('DJANGO_DEBUG', False)
 
 #ALLOWED_HOSTS = ['vchip.app', '68.183.19.183']
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*,vchip.merrell.io').split(',')
+# ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*,vchip.merrell.io').split(',')
 
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
