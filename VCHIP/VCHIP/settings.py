d714abf99caba0d147f72f7cd9316261e3ed91ee
Justin <merrelljustin@gmail.com>
2022-02-09 21:13:27 +0000

    Static files

--- VCHIP/VCHIP/settings.py
+++ VCHIP/VCHIP/settings.py
@@ -157,8 +157,9 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
-STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-STATIC_ROOT = 'static/'
+# STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/vchip/static/'
 
 # Logging Configuration
 
